42c9c191a9872093dfb24ac0b926f6bf47b51685
Harold Seo <harold.seo.612@gmail.com>
2021-11-17 15:50:30 -0800

    Core Types

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("hello");
+// Core Types: Number, String, Boolean
+function add(n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
+}
+var number1 = 5;
+var number2 = 2.8;
+var result = true;
+var resultPhrase = "Result is: ";
+add(number1, number2, result, resultPhrase);
