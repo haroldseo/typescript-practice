712456b26b981ccdbf26a00622045d554f1b325c
Harold Seo <harold.seo.612@gmail.com>
2021-11-19 15:59:00 -0800

    Tuple Type

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var person = {
     name: "Harold",
     age: 29,
-    hobbies: ["sports", "photography"]
+    hobbies: ["sports", "photography"],
+    role: [2, "author"]
 };
-console.log(person.name);
+// person.role[1] = 10;
+// person.role = [1, "hello", "bye"];
+// person.role.push("admin");
+console.log(person.role);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
--- app.ts
+++ app.ts
@@ -1,10 +1,20 @@
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "Harold",
   age: 29,
   hobbies: ["sports", "photography"],
+  role: [2, "author"],
 };
 
-console.log(person.name);
+// person.role[1] = 10;
+// person.role = [1, "hello", "bye"];
+// person.role.push("admin");
+
+console.log(person.role);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
