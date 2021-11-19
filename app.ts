be497eac719e8c3d44326590163779e072654031
Harold Seo <harold.seo.612@gmail.com>
2021-11-18 17:17:15 -0800

    Array Type

--- app.ts
+++ app.ts
@@ -1,6 +1,11 @@
 const person = {
   name: "Harold",
   age: 29,
+  hobbies: ["sports", "photography"],
 };
 
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
