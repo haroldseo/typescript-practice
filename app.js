80bfbdc6233fd5754e272a6b8f0bfbb4467919a2
Harold Seo <harold.seo.612@gmail.com>
2021-11-23 17:13:01 -0800

    Literal Type

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultType) {
     var result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
+    if (resultType === "as-number") {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
+    //   if (resultType === "as-number") {
+    //     return +result;
+    //   } else {
+    //     return result.toString();
+    //   }
 }
-var combinedAges = combine(30, 30);
+var combinedAges = combine(20, 30, "as-number");
 console.log(combinedAges);
-var combinedNames = combine("Joe", "Anna");
+var combineStringToNumber = combine("10", "20", "as-number");
+console.log(combineStringToNumber);
+var combineNumberToString = combine(40, 80, "as-string");
+console.log(combineNumberToString);
+var combinedNames = combine("Joe", "Anna", "as-string");
 console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,15 +1,26 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(input1: number | string, input2: number | string, resultType: "as-number" | "as-string") {
   let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (resultType === "as-number") {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
   return result;
+  //   if (resultType === "as-number") {
+  //     return +result;
+  //   } else {
+  //     return result.toString();
+  //   }
 }
 
-const combinedAges = combine(20, 30);
+const combinedAges = combine(20, 30, "as-number");
 console.log(combinedAges);
 
-const combinedNames = combine("Joe", "Anna");
+const combineStringToNumber = combine("10", "20", "as-number");
+console.log(combineStringToNumber);
+
+const combineNumberToString = combine(40, 80, "as-string");
+console.log(combineNumberToString);
+
+const combinedNames = combine("Joe", "Anna", "as-string");
 console.log(combinedNames);
