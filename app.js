5b4fb16bf5c948c80b3437f9f543ca1119395071
Harold Seo <harold.seo.612@gmail.com>
2021-11-30 15:38:51 -0800

    Function Types

--- app.js
+++ app.js
@@ -8,3 +8,8 @@ function printResult(num) {
     console.log("Result: " + num);
 }
 printResult(add(5, 12));
+// let combineValues: Function;
+var combineValues;
+combineValues = add;
+//combineValues = printResult; Error
+console.log(combineValues(8, 8));
--- app.ts
+++ app.ts
@@ -10,3 +10,11 @@ function printResult(num: number) {
 }
 
 printResult(add(5, 12));
+
+// let combineValues: Function;
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+//combineValues = printResult; Error
+
+console.log(combineValues(8, 8));
