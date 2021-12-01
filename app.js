d5fffc9f1d36a0492c110bbd01aa23a5ad129f1c
Harold Seo <harold.seo.612@gmail.com>
2021-12-01 14:13:10 -0800

    Function Types and Callbacks

--- app.js
+++ app.js
@@ -13,3 +13,11 @@ var combineValues;
 combineValues = add;
 //combineValues = printResult; Error
 console.log(combineValues(8, 8));
+//Function Types and Callbacks
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
--- app.ts
+++ app.ts
@@ -13,8 +13,16 @@ printResult(add(5, 12));
 
 // let combineValues: Function;
 let combineValues: (a: number, b: number) => number;
-
 combineValues = add;
 //combineValues = printResult; Error
-
 console.log(combineValues(8, 8));
+
+//Function Types and Callbacks
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
